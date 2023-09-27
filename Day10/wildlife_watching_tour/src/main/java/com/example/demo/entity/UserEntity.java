ce0593246b33b2949ec36701403994e1e41088f4
Abi Chellappa <119254315+Abi-Chellappa@users.noreply.github.com>
2023-09-27 13:27:10 +0530

    Add files via upload

---
+++ Day10/wildlife_watching_tour/src/main/java/com/example/demo/entity/UserEntity.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity 
+{
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   private int userId;
+   private String userName;
+   private String password;
+    
+}
