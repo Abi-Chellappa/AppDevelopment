899b05a729a66ceaa7faa1dafe1e3fa6a6950555
Abi Chellappa <119254315+Abi-Chellappa@users.noreply.github.com>
2023-10-11 02:31:03 -0700

    Add files via upload

---
+++ Day18/wildlife/src/main/java/com/example/demo/service/FeedbackService.java
@@ -0,0 +1,27 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Feedback;
+import com.example.demo.repository.FeedbackRepository;
+
+@Service
+public class FeedbackService 
+{
+	@Autowired
+	private FeedbackRepository feedbackRepo;
+
+	public List<Feedback> getFeedback() 
+	{
+		return feedbackRepo.findAll();
+	}
+
+	public void saveFeedback(Feedback feedback) 
+	{
+		feedbackRepo.save(feedback);
+	}
+   
+}
